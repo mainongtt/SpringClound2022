4e76f4334d544506459b82c7c9535d6a3bd4e86b
zk <1819592017@qq.com>
2022-07-25 11:16:43 +0800

    整合注册中心consul

---
+++ 007-cloud-consumerzk-order80/src/main/java/com/example/OrderPayment80Application.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPayment80Application {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderPayment80Application.class, args);
+    }
+}
