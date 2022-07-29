d01684d2907b1aabd7441a784a17348b43ea3568
zk <1819592017@qq.com>
2022-07-29 15:52:56 +0800

    整合Hystrix熔断

--- 011-cloud-provider-hystrix-payment8001/src/main/java/com/example/service/PaymentService.java
+++ 011-cloud-provider-hystrix-payment8001/src/main/java/com/example/service/PaymentService.java
@@ -1,9 +1,11 @@
 package com.example.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -33,5 +35,28 @@ public class PaymentService {
     public String paymentInfo_TimeOutHandler(Integer id){
         log.info("触发兜底方法");
         return "触发兜底方法";
+    }
+
+    // ===================服务熔断
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"), // 是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"), // 对服务的调用次数，默认20
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"), // 时间窗口期（总请求的时间）10s
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"), // 失败率达到60%后跳闸
+            // 在 10s 中，总共请求次数是10次，超过60%（6次请求），断路器起作用
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id)
+    {
+        if(id < 0)
+        {
+            throw new RuntimeException("******id 不能负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+
+        return Thread.currentThread().getName()+"/t"+"调用成功，流水号: " + serialNumber;
+    }
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id)
+    {
+        return "id 熔断降级了   id: " +id;
     }
 }
