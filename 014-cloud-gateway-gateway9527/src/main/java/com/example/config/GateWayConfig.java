e1a4a734c9380a04f19eaccefc33c345b7f398ac
zk <1819592017@qq.com>
2022-08-01 15:40:27 +0800

    整合GateWay网关

---
+++ 014-cloud-gateway-gateway9527/src/main/java/com/example/config/GateWayConfig.java
@@ -0,0 +1,18 @@
+package com.example.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // 访问当前 path 转接到 uri
+                .route("resource", r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei"))
+                .build();
+    }
+}
---
+++ 014-cloud-gateway-gateway9527/src/main/java/com/example/filter/MyLogGlobalGateWayFilter.java
@@ -0,0 +1,32 @@
+package com.example.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class MyLogGlobalGateWayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("MyLogGlobalGateWayFilter 开始启动：");
+        String unmae = exchange.getRequest().getQueryParams().getFirst("unmae");
+        if(unmae == null){
+            System.out.println("用户名为空：");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    // 设置过滤器的优先级 越小越靠前
+    public int getOrder() {
+        return 0;
+    }
+}
