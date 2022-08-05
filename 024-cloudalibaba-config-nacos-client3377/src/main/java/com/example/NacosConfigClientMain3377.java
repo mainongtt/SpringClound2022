aea66167786f6fc8d34041d75b9efa8825194344
zk <1819592017@qq.com>
2022-08-05 16:44:28 +0800

    整合阿里的nacos

---
+++ 024-cloudalibaba-config-nacos-client3377/src/main/java/com/example/NacosConfigClientMain3377.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
